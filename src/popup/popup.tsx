0df093b8f5792c48d2235a6bf970735770fe0b8c
Kartik Krishnan <79468373+fitnessforall@users.noreply.github.com>
2022-10-04 08:25:48 +0200

    Updates

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../assets/tailwind.css'
 import { createRoot } from 'react-dom/client'
+//import check from '../static/images/check.png'
+//import get from '../static/images/get.png'
+//import save from '../static/images/save.png'
 
 const App = (
-    
         <>
         <div className="relative w-auto h-20 m-[20px] bg-white rounded-[20px] border-emerald-900 border-4">
             <div className=" absolute w-full top-[-5px] font-sans text-center text-[50px]"> 
@@ -16,15 +18,21 @@ const App = (
             </div>
         </div>
         <div className="absolute w-[75px] h-[257px] left-[25px] top-[125px]">
+            <a href="" className="cursor-pointer">
             <div className="w-[75px] h-[75px] bg-white rounded-[20px] mb-[16px] hover:scale-105 border-emerald-900 border-4">
             <img src="check" alt="check" className=""/>
             </div>
+            </a>
+            <a href="" className="cursor-pointer">
             <div className="w-[75px] h-[75px] bg-white rounded-[20px] mb-[16px] hover:scale-105 border-emerald-900 border-4">
-
+            <img src="get" alt="get" className=""/>
             </div>
+            </a>
+            <a href="" className="cursor-pointer">
             <div className="w-[75px] h-[75px] bg-white rounded-[20px] hover:scale-105 border-emerald-900 border-4">
-
+            <img src="save" alt="save" className=""/>
             </div>
+            </a>
         </div>
         <div className="absolute w-[95px] h-[50px] left-[145px] top-[332px] bg-white rounded-[20px] hover:scale-105 border-emerald-900 border-4">
             <div className="absolute text-center w-full top-[10px] text-base font-semibold"> Login </div>
