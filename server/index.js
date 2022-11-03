fa91fadcb04a3c9093a31f8567bdd23898299edd
P3ntest <33764638+P3ntest@users.noreply.github.com>
2022-11-03 12:11:34 +0100

    WIP dockerize

--- server/index.js
+++ server/index.js
@@ -1,72 +1,80 @@
-const express = require('express')
-const cors = require("cors")
-const app = express()
-const mysql = require('mysql')
+require("dotenv").config();
 
-const bcrypt = require('bcrypt')
-const { response } = require('express')
-const saltRounds = 10
-const Pepper = "Yoru"
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+const bcrypt = require("bcrypt");
+const { response } = require("express");
+const saltRounds = 10;
+const Pepper = process.env.PEPPER;
 
 const dbconnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Kkottobrunn2013.',
-  database: 'sagiouspass'
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send("In Server")
-})
+app.get("/", (req, res) => {
+  res.send("In Server");
+});
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
+  const Username = req.body.Username;
+  const Password = Pepper + req.body.Password;
 
-  const Username = req.body.Username
-  const Password = Pepper + req.body.Password
+  const FavWord = req.body.FavWord;
+  const FavSymbol = req.body.FavSymbol;
 
-  const FavWord = req.body.FavWord
-  const FavSymbol = req.body.FavSymbol
-
-  dbconnection.query("SELECT * FROM useraccount WHERE ? IN (Username);",
+  dbconnection.query(
+    "SELECT * FROM useraccount WHERE ? IN (Username);",
     Username,
     (err, response) => {
       //console.log(response)
       if (response.length != 0) {
-        res.send({ message: "Username already exists, choose a different one" })
+        res.send({
+          message: "Username already exists, choose a different one",
+        });
       } else {
-        dbconnection.query("SELECT * FROM useraccount WHERE Username = ?;",
+        dbconnection.query(
+          "SELECT * FROM useraccount WHERE Username = ?;",
           Username,
           (err, response) => {
             if (err) {
-              console.log(err)
+              console.log(err);
             } else {
               bcrypt.hash(Password, saltRounds, (err, hash) => {
                 if (err) {
-                  console.log(err)
+                  console.log(err);
                 }
-                dbconnection.query("INSERT INTO useraccount (Username, Password, FavWord, FavSymbol) VALUES (?,?,?,?)",
+                dbconnection.query(
+                  "INSERT INTO useraccount (Username, Password, FavWord, FavSymbol) VALUES (?,?,?,?)",
                   [Username, hash, FavWord, FavSymbol],
                   (err, result) => {
                     console.log(err);
-                  })
-              })
+                  }
+                );
+              });
             }
-          })
-        res.send(response)
+          }
+        );
+        res.send(response);
       }
     }
-  )
-})
+  );
+});
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
+  const Username = req.body.Username;
+  const Password = Pepper + req.body.Password;
 
-  const Username = req.body.Username
-  const Password = Pepper + req.body.Password
-
-  dbconnection.query("SELECT * FROM useraccount WHERE Username = ?;",
+  dbconnection.query(
+    "SELECT * FROM useraccount WHERE Username = ?;",
     Username,
     (err, result) => {
       if (err) {
@@ -75,167 +83,210 @@ app.post('/login', (req, res) => {
       if (result.length > 0) {
         bcrypt.compare(Password, result[0].Password, (error, response) => {
           if (response) {
-            res.send(response)
+            res.send(response);
           } else {
-            res.send({ message: "Wrong username/password combination" })
+            res.send({ message: "Wrong username/password combination" });
           }
-        })
+        });
       } else {
-        res.send({ message: "User can not be found" })
+        res.send({ message: "User can not be found" });
       }
-    })
-})
+    }
+  );
+});
 
-app.post('/getFavWord&Symbol', (req, res) => {
+app.post("/getFavWord&Symbol", (req, res) => {
+  const Username = req.body.Username;
 
-  const Username = req.body.Username
-
-  dbconnection.query("SELECT * FROM useraccount WHERE Username = ?;",
+  dbconnection.query(
+    "SELECT * FROM useraccount WHERE Username = ?;",
     Username,
     (err, result) => {
-      res.send(Object.values(JSON.parse(JSON.stringify(result))))
+      res.send(Object.values(JSON.parse(JSON.stringify(result))));
     }
-  )
-})
+  );
+});
 
-app.post('/save', (req, res) => {
-  const Username = req.body.Username
+app.post("/save", (req, res) => {
+  const Username = req.body.Username;
   const url = req.body.urlSave;
-  const password = req.body.passwordSave
+  const password = req.body.passwordSave;
 
-  dbconnection.query("SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
+  dbconnection.query(
+    "SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
     Username,
     (err, response) => {
-      const id = (Object.values(JSON.parse(JSON.stringify(response)))[0].idUserAccount)
+      const id = Object.values(JSON.parse(JSON.stringify(response)))[0]
+        .idUserAccount;
       //console.log(id)
-      dbconnection.query("SELECT * FROM savedpasswords WHERE ? IN (Url) AND ? IN (UserId);",
+      dbconnection.query(
+        "SELECT * FROM savedpasswords WHERE ? IN (Url) AND ? IN (UserId);",
         [url, id],
         (err, response) => {
           if (response.length != 0) {
-            res.send({ message: "Url already has a saved password, if you want to change it please Update it" })
+            res.send({
+              message:
+                "Url already has a saved password, if you want to change it please Update it",
+            });
           } else {
-            dbconnection.query("INSERT INTO savedpasswords (UserId, Url, password ) VALUES (?,?,?);",
+            dbconnection.query(
+              "INSERT INTO savedpasswords (UserId, Url, password ) VALUES (?,?,?);",
               [id, url, password],
               (err, response) => {
-                res.send(response)
+                res.send(response);
               }
-            )
+            );
           }
-        })
-    })
-})
+        }
+      );
+    }
+  );
+});
 
-app.post('/update', (req, res) => {
-  const Username = req.body.Username
+app.post("/update", (req, res) => {
+  const Username = req.body.Username;
   const url = req.body.urlSave;
-  const password = req.body.passwordSave
+  const password = req.body.passwordSave;
 
-  dbconnection.query("SELECT idsavedpasswords FROM savedpasswords WHERE ? IN (Url);",
+  dbconnection.query(
+    "SELECT idsavedpasswords FROM savedpasswords WHERE ? IN (Url);",
     url,
     (err, response) => {
-      const id = (Object.values(JSON.parse(JSON.stringify(response)))[0].idsavedpasswords)
-      dbconnection.query("UPDATE savedpasswords SET password = ? WHERE idsavedpasswords = ?;",
+      const id = Object.values(JSON.parse(JSON.stringify(response)))[0]
+        .idsavedpasswords;
+      dbconnection.query(
+        "UPDATE savedpasswords SET password = ? WHERE idsavedpasswords = ?;",
         [password, id],
         (err, response) => {
-          res.send({ message: "Password was changed for: " + url })
-        })
+          res.send({ message: "Password was changed for: " + url });
+        }
+      );
     }
-  )
-})
+  );
+});
 
-app.post('/find', (req, res) => {
-  const searchurl = req.body.url
+app.post("/find", (req, res) => {
+  const searchurl = req.body.url;
   const Username = req.body.username;
 
-  dbconnection.query("SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
+  dbconnection.query(
+    "SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
     Username,
     (err, response) => {
-      const id = (Object.values(JSON.parse(JSON.stringify(response)))[0].idUserAccount)
-      dbconnection.query("SELECT * FROM savedpasswords WHERE ? IN (Url) AND ? IN (UserId) ;",
+      const id = Object.values(JSON.parse(JSON.stringify(response)))[0]
+        .idUserAccount;
+      dbconnection.query(
+        "SELECT * FROM savedpasswords WHERE ? IN (Url) AND ? IN (UserId) ;",
         [searchurl, id],
         (err, response) => {
           if (response.length != 0) {
-            dbconnection.query("SELECT password FROM savedpasswords WHERE ? IN (Url) AND ? IN (UserId);",
+            dbconnection.query(
+              "SELECT password FROM savedpasswords WHERE ? IN (Url) AND ? IN (UserId);",
               [searchurl, id],
               (err, response) => {
                 if (err) {
                   res.send({ message: "No url available" });
                 }
-                res.send(Object.values(JSON.parse(JSON.stringify(response)))[0].password);
-              })
+                res.send(
+                  Object.values(JSON.parse(JSON.stringify(response)))[0]
+                    .password
+                );
+              }
+            );
           } else {
-            res.send({ message: "No url found!" })
+            res.send({ message: "No url found!" });
           }
         }
-      )
-    })
-})
+      );
+    }
+  );
+});
 
-app.post('/changefavword', (req, res) => {
-  const favWord = req.body.FavWord
-  const username = req.body.Username
+app.post("/changefavword", (req, res) => {
+  const favWord = req.body.FavWord;
+  const username = req.body.Username;
 
-  dbconnection.query("SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
+  dbconnection.query(
+    "SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
     username,
     (err, response) => {
-      const id = (Object.values(JSON.parse(JSON.stringify(response)))[0].idUserAccount)
-      dbconnection.query("UPDATE useraccount SET FavWord = ? WHERE idUserAccount = ?;",
+      const id = Object.values(JSON.parse(JSON.stringify(response)))[0]
+        .idUserAccount;
+      dbconnection.query(
+        "UPDATE useraccount SET FavWord = ? WHERE idUserAccount = ?;",
         [favWord, id],
         (err, response) => {
-          res.send({ message: "Favourite Word was changed to: " + favWord })
-        })
-    })
-})
+          res.send({ message: "Favourite Word was changed to: " + favWord });
+        }
+      );
+    }
+  );
+});
 
-app.post('/changefavsymbol', (req, res) => {
-  const favSmybol = req.body.FavSymbol
-  const username = req.body.Username
+app.post("/changefavsymbol", (req, res) => {
+  const favSmybol = req.body.FavSymbol;
+  const username = req.body.Username;
 
-  dbconnection.query("SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
+  dbconnection.query(
+    "SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
     username,
     (err, response) => {
-      const id = (Object.values(JSON.parse(JSON.stringify(response)))[0].idUserAccount)
-      dbconnection.query("UPDATE useraccount SET FavSymbol = ? WHERE idUserAccount = ?;",
+      const id = Object.values(JSON.parse(JSON.stringify(response)))[0]
+        .idUserAccount;
+      dbconnection.query(
+        "UPDATE useraccount SET FavSymbol = ? WHERE idUserAccount = ?;",
         [favSmybol, id],
         (err, response) => {
-          res.send({ message: "Favourite Symbol was changed to: " + favSmybol })
-        })
-    })
-})
+          res.send({
+            message: "Favourite Symbol was changed to: " + favSmybol,
+          });
+        }
+      );
+    }
+  );
+});
 
-app.post('/changepassword', (req, res) => {
-  const newPassword = Pepper + req.body.NewPassword
-  const username = req.body.Username
+app.post("/changepassword", (req, res) => {
+  const newPassword = Pepper + req.body.NewPassword;
+  const username = req.body.Username;
 
-  dbconnection.query("SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
+  dbconnection.query(
+    "SELECT idUserAccount FROM useraccount WHERE ? IN (Username);",
     username,
     (err, response) => {
-      const id = (Object.values(JSON.parse(JSON.stringify(response)))[0].idUserAccount)
+      const id = Object.values(JSON.parse(JSON.stringify(response)))[0]
+        .idUserAccount;
       bcrypt.genSalt(saltRounds, (err, salt) => {
         bcrypt.hash(newPassword, salt, (err, hash) => {
-          dbconnection.query("UPDATE useraccount SET Password = ? WHERE idUserAccount = ?;",
+          dbconnection.query(
+            "UPDATE useraccount SET Password = ? WHERE idUserAccount = ?;",
             [hash, id],
             (err, response) => {
-              res.send({ message: "Password was changed to: " + hash })
-            })
-        })
-      })
-    })
-})
+              res.send({ message: "Password was changed to: " + hash });
+            }
+          );
+        });
+      });
+    }
+  );
+});
 
-app.post('/deleteuser', (req, res) => {
-  const username = req.body.Username
-  dbconnection.query("DELETE FROM useraccount WHERE ? IN (Username) ;",
+app.post("/deleteuser", (req, res) => {
+  const username = req.body.Username;
+  dbconnection.query(
+    "DELETE FROM useraccount WHERE ? IN (Username) ;",
     username,
     (err, response) => {
-      res.send({ message: "User: " + username + " was deleted succesfully" })
-    })
-})
+      res.send({ message: "User: " + username + " was deleted succesfully" });
+    }
+  );
+});
 
-app.listen(3000, () => {
-  console.log("running server");
-})
+const PORT = process.env.PORT ?? 3e3;
 
+app.listen(PORT, () => {
+  console.log("running server on port", PORT);
+});
 
 /* 
 dbconnection.connect((error) => {
